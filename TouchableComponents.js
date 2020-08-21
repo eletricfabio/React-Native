bc49dd69321f0472aad43583d4ab1ffd0414b942
Fábio Vieira <43624576+eletricfabio@users.noreply.github.com>
2020-08-21 01:36:07 -0300

    TouchableComponents

---
+++ TouchableComponents.js
@@ -0,0 +1,66 @@
+//Aula8
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'Fábio', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id != id);
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      
+      <FlatList
+      numColumns={2}
+      keyExtractor={(item) => item.id}
+          data={people}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => pressHandler(item.id)}>
+              <Text style={styles.item}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+      />
+    
+    {/*<ScrollView>
+    { people.map(item => (
+        <View key={item.key}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>
+    ))}
+    </ScrollView>*/}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  paddingTop: 40,
+  paddingHorizontal: 20,
+  //alignItems: 'center',
+  //justifyContent: 'center',
+},
+item: {
+  marginTop: 24,
+  padding: 30,
+  backgroundColor: 'orange',
+  fontSize: 24,
+  marginHorizontal: 10,
+  marginTop: 24,
+}
+});
